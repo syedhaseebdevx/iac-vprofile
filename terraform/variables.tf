b838f1fe60fb95f9666f68a12c4520d8d64c5728
syedhaseebdevx <haseeb.devx@gmail.com>
2025-11-05 16:02:07 +0500

    First stage

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,11 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "gitops"
+  default     = "vprofile-eks"
 }
-##
